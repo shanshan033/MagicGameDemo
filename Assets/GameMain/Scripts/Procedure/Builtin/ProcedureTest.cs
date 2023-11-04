e0bb13b9dd01e6cb687e718fad91ef46cdee112e
cancantrbl <lishanshan.unnc@aiesec.net>
2023-11-04 12:45:39 +0800

    UI初始界面

---
+++ Assets/GameMain/Scripts/Procedure/Builtin/ProcedureTest.cs
@@ -0,0 +1,19 @@
+﻿using GameFramework.Procedure;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+using GameFramework;
+
+namespace GDT
+{
+    /// <summary>
+    /// 测试流程
+    /// </summary>
+    public class ProcedureTest : ProcedureBase
+    {
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            Log.Info("This is test procedure");
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+        }
+    }
+}
